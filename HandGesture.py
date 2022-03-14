613b00a037a5266ca97f3d1699094cd2b38e2752
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-03-15 05:52:30 +0700

    show FPS

--- HandGesture.py
+++ HandGesture.py
@@ -1,17 +1,26 @@
 import cv2
+import time
+import mediapipe as mp
 
+pre_frame_time = 0
+new_frame_time = 0
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 cam = cv2.VideoCapture(0)
 
-
 while True:
-    result, image = cam.read()
+    sussess, image = cam.read()
+    image = cv2.flip(image, 1)
     
-    cv2.imshow("hand Detection", image)
+    # Calculate FPS
+    new_frame_time = time.time()
+    FPS = 1/(new_frame_time - pre_frame_time)
+    pre_frame_time = new_frame_time
+    cv2.putText(image, "FPS: "+str(int(FPS)), (0, 40), font, 1, (100, 255, 0), 3, cv2.LINE_AA)
 
+    cv2.imshow('Thanh HoangVan - Hand Gesture Recognition', image)
     k = cv2.waitKey(1)
-    # ESC pressed -> quit
     if k%256 == 27:
         break
 
-cv2.destroyWindow("GeeksForGeeks")
+cv2.destroyAllWindows()
